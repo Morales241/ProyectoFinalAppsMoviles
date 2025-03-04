be0e7bb88fdf3e01b3336f7541e647abee7392b6
RobertoFavela <160526441+RobertoFavela@users.noreply.github.com>
2025-03-03 22:42:37 -0700

    Registro avance
    Creacion de componentes basicos de pantalla de registro

--- ClosetVitual/app/src/main/java/morales/jesus/closetvitual/registrarse.kt
+++ ClosetVitual/app/src/main/java/morales/jesus/closetvitual/registrarse.kt
@@ -1,6 +1,9 @@
 package morales.jesus.closetvitual
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,21 @@ class registrarse : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // redirige a IniciarSesion
+        val btnRegistrarse = findViewById<Button>(R.id.btn_registrarse)
+        btnRegistrarse.setOnClickListener {
+            val intent = Intent(this, IniciarSesion::class.java)
+            startActivity(intent)
+        }
+
+        // redirige a IniciarSesion
+        val tvIrAInicioSesion = findViewById<TextView>(R.id.tv_ir_a_inicio_sesion)
+        tvIrAInicioSesion.setOnClickListener {
+            val intent = Intent(this, IniciarSesion::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
